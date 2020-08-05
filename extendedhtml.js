4e27507125c1e181c66baeaae5fe9cb75bee4d68
Mohammed Zahid Wadiwale <63946803+ZahidServers@users.noreply.github.com>
2020-08-05 05:02:51 -0700

    New Updated Indicators added to Slider
    New Updated Indicators added to Slider also licences comment added to js

--- extendedhtml.js
+++ extendedhtml.js
@@ -1,16 +1,30 @@
-		var slideIndex = 1;
-		function showDivs(n) {
-		  var i;
-		  var x = document.getElementsByClassName("slider-slides");
-		  if (n > x.length) {slideIndex = 1}
-		  if (n < 1) {slideIndex = x.length}
-		  for (i = 0; i < x.length; i++) {
-		    x[i].style.display = "none";  
-		  }
-		  x[slideIndex-1].style.display = "block";  
-		}
-		showDivs(slideIndex);
-
-		function plusDivs(n) {
-		  showDivs(slideIndex += n);
-		}
+/*
+* @package Extentended HTML 
+* @version 1.0.0 
+* @author Extentended HTML Mohammed Zahid Wadiwale
+* @copyright Copyright (c) 2020 Mohammed Zahid Wadiwale 
+* @license - zahidservers.github.io/Extended-HTML/license.md
+*/
+var slideIndex = 1;
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showDivs(n) {
+  var i;
+  var dots = document.getElementsByClassName("dot");
+  var x = document.getElementsByClassName("slider-slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
